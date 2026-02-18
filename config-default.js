6e99b15f2913860c4631d18011666d43390ac8f8
digital-invita <anuar.rios.310@gmail.com>
2026-02-18 07:11:39 -0600

    Crear config-default.js

---
+++ config-default.js
@@ -0,0 +1,13 @@
+const DEFAULT_CONFIG = {
+  nombre: "Sofía Montes",
+  fecha: "15 de Marzo 2026",
+  telefono: "525550591264",
+  colorPrincipal: "#d4af37",
+  musica: "musica.mp3",
+  galeria: [],
+  secciones: {
+    galeria: true,
+    confirmacion: true,
+    contador: true
+  }
+};
