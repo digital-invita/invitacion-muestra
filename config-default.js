091f3423268e9fdd1d944de66e4f5a511aa465af
digital-invita <anuar.rios.310@gmail.com>
2026-02-18 16:49:45 -0600

    Update config-default.js

--- config-default.js
+++ config-default.js
@@ -6,15 +6,20 @@ window.CONFIG = {
   portada: "portada.jpg",
   fotoHistoria: "FHistoria.jpg",
 
-  galeria: ["G1.jpg","G2.jpg","G3.jpg","G4.jpg","G5.jpg","G6.jpg"],
+  galeria: [
+    "G1.jpg",
+    "G2.jpg",
+    "G3.jpg",
+    "G4.jpg",
+    "G5.jpg",
+    "G6.jpg"
+  ],
 
   vestimentaImg: "vestimenta.png",
 
-  musica: "musica.mp3",
-
   textos: {
-    historia1: "Hoy celebro no solo mis 15 años...",
-    historia2: "Aquí termina un capítulo...",
+    historia1: "Hoy celebro no solo mis 15 años, sino cada sonrisa que iluminó mi camino, cada aprendizaje que fortaleció mi corazón y cada sueño que, con amor y esperanza, me ha traído hasta este momento tan especial de mi vida.",
+    historia2: "Aquí termina un capítulo lleno de recuerdos que atesoraré por siempre… y comienza uno nuevo, donde la ilusión, la luz y los sueños por cumplir marcan el inicio de todo lo que está por venir.",
     padres: "María González<br>&<br>Carlos Montes",
     padrinos: "Laura Pérez<br>&<br>José Hernández"
   }
