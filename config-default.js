bdc94925ceba71a25f76b8c115d6e529f2af52c7
digital-invita <anuar.rios.310@gmail.com>
2026-02-18 16:26:31 -0600

    Update config-default.js

--- config-default.js
+++ config-default.js
@@ -1,7 +1,22 @@
 window.CONFIG = {
-  nombre: "Valeria López",
-  fecha: "20 de agosto de 208",
 
-  portada: "FPrincipal.jpg",
-  musica: "cancio.mp3.mp3"
+  nombre: "Sofía Montes",
+  fecha: "15 de agosto de 2026",
+
+  portada: "portada.jpg",
+  fotoHistoria: "FHistoria.jpg",
+
+  galeria: ["G1.jpg","G2.jpg","G3.jpg","G4.jpg","G5.jpg","G6.jpg"],
+
+  vestimentaImg: "vestimenta.png",
+
+  musica: "musica.mp3",
+
+  textos: {
+    historia1: "Hoy celebro no solo mis 15 años...",
+    historia2: "Aquí termina un capítulo...",
+    padres: "María González<br>&<br>Carlos Montes",
+    padrinos: "Laura Pérez<br>&<br>José Hernández"
+  }
+
 };
